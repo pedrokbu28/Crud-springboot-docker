1b4eb3a7aca5cf8593514f335cd71c0043534040
pedrokbu28 <pedrokbu@hotmail.com>
2023-06-27 14:45:07 -0300

    troca de senha do mysql e flyway e troca de ver mysql

---
+++ chavepix/src/test/java/github/com/pedrokbu28/ControllerApplicationTests.java
@@ -0,0 +1,25 @@
+// package github.com.pedrokbu28;
+
+// import java.util.List;
+
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.extension.ExtendWith;
+// import org.mockito.InjectMocks;
+// import org.mockito.junit.jupiter.MockitoExtension;
+
+// import github.com.pedrokbu28.controller.controladorChavepix;
+// import github.com.pedrokbu28.model.ChavepixApplication;
+
+// @ExtendWith(MockitoExtension.class)
+// public class ChavepixApplicationTests {
+    
+//     InjectMocks
+//     private controladorChavepix ControladorChavepix;
+    
+//     private ChavepixIn chavepixIn;
+
+//     @BeforeEach
+//     void setup() {
+//         chavepixIn = new ChavepixIn(telefone "(34)123456789", cpf "123456789011");
+//     }
+// }
